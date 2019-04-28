3a3c41ade4af4d01a43709b1d31d8d05ffaebaa4
renatoathaydes <renatoathaydes@hotmail.com>
2019-04-28 19:01:45 +0200

    CheckBox - minor fixes.

--- lib/src/widgets/checkbox.dart
+++ lib/src/widgets/checkbox.dart
@@ -3,25 +3,30 @@ import 'dart:html';
 import '../base.dart';
 
 /// Checkbox widget.
+///
+/// It wraps a [CheckboxInputElement] and a [LabelElement]
+/// inside a [SpanElement].
 class Checkbox with Widget {
   final _input = CheckboxInputElement();
   final _label = LabelElement();
 
-  Checkbox({String name = '',
-    String label = '',
-    bool checked = false,
-    Function(bool) onChange}) {
-    _input
-      ..name = name
-      ..checked = checked;
+  Checkbox(
+      {String id = '',
+      String label = '',
+      bool checked = false,
+      Function(bool) onChange}) {
+    if (id != null) {
+      _input.id = id;
+      _input.name = id;
+      _label.htmlFor = id;
+    }
+    _input.checked = checked;
     if (onChange != null) {
       _input.onChange.listen((e) => onChange(_input.checked));
     }
-    _label..setAttribute('for', name)..text = label;
+    _label.text = label;
   }
 
   @override
-  Element build() =>
-      SpanElement()
-        ..append(_input)..append(_label);
+  Element build() => SpanElement()..append(_input)..append(_label);
 }
