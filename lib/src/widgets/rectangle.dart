4951481a6eab2690a360c7894842b509ab625f35
renatoathaydes <renatoathaydes@hotmail.com>
2019-05-01 22:26:46 +0200

    Improved Rectangle widget.

--- lib/src/widgets/rectangle.dart
+++ lib/src/widgets/rectangle.dart
@@ -7,8 +7,8 @@ class Rectangle with Widget {
   final Element _element = DivElement()..style.position = 'relative';
 
   Rectangle(
-      {String width = '10px',
-      String height = '10px',
+      {String width = '100px',
+      String height = '50px',
       String top = '0',
       String left = '0',
       String fill = 'white',
@@ -16,6 +16,22 @@ class Rectangle with Widget {
     _element.style
       ..width = width
       ..height = height
+      ..backgroundColor = fill
+      ..border = border
+      ..top = top
+      ..left = left;
+  }
+
+  /// Create a square.
+  Rectangle.square(
+      {String size = '100px',
+      String top = '0',
+      String left = '0',
+      String fill = 'white',
+      String border = 'solid black 1px'}) {
+    _element.style
+      ..width = size
+      ..height = size
       ..backgroundColor = fill
       ..border = border
       ..top = top
