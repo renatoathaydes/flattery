f771d3db99d88b91c9e7c8d257ce0b937f2aeb5c
renatoathaydes <renatoathaydes@hotmail.com>
2019-05-01 22:29:25 +0200

    Updated gallery to include Grid. Improved its layout.

--- example/gallery.dart
+++ example/gallery.dart
@@ -26,25 +26,59 @@ class Gallery with Widget, ShadowWidget {
             label: 'Highlight Flattery Widgets',
             onChange: highlightWidgets,
           ),
-          Column(children: [
-            Text(text: 'Text Widget'),
-            Checkbox(id: 'checkbox-example', label: 'CheckBox Widget'),
-            Button(text: 'Button Widget'),
-            Row(justify: JustifyContent.spaceEvenly, children: [
-              Text(text: 'This is'),
-              Text(text: 'a Row Widget'),
-              Text(text: 'with 4 Text items'),
-              Text(text: 'spaced evenly'),
-            ]),
-            Rectangle(width: '5em', height: '5em', fill: 'red', left: '24px'),
-          ])
+          Grid(
+              //
+              columnWidths: [
+                '10em', 'auto' //
+              ],
+              rowGap: '10px',
+              children: [
+                [
+                  header('Widget Type'),
+                  header('Example')..style.borderLeft = 'solid 1px black'
+                ],
+                [Text('Text'), Text('I am a Text Widget')],
+                [
+                  Text('Checkbox'),
+                  Checkbox(id: 'checkbox-example', label: 'Click to toggle')
+                ],
+                [Text('Button'), Button(text: 'Click me!')],
+                [
+                  Text('Row'),
+                  Row(justify: JustifyContent.spaceEvenly, children: [
+                    Text('This is'),
+                    Text('a Row'),
+                    Text('with 4 Text items'),
+                    Text('spaced evenly'),
+                  ])
+                ],
+                [Text('Rectangle'), Rectangle(fill: 'red', left: '2em')],
+                [
+                  Text('Grid'),
+                  Grid(columnWidths: [
+                    '3em', '3em', '5em' //
+                  ], rowHeights: [
+                    '3em', '3em' //
+                  ], children: [
+                    [
+                      square('blue'),
+                      square('green'),
+                      Rectangle(width: '2em', height: '5em', fill: 'yellow')
+                    ],
+                    [square('red'), square('purple')]
+                  ])
+                ],
+              ])
             ..style.border = 'solid black 1px'
             ..style.marginTop = '1em'
-            ..style.padding = '1em',
         ],
       ).root;
 }
 
-Element div(Widget w) => DivElement()..append(w.root);
+Widget header(String text) => Text(text)
+  ..style.backgroundColor = 'lightgray'
+  ..style.borderBottom = 'black solid 1px';
+
+Widget square(String color) => Rectangle.square(size: '2em', fill: color);
 
 main() => document.getElementById('output').append(Gallery().root);
