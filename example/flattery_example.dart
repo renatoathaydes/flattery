d8c5c4695de12a5c81df1510fe98eb4ceafb063b
renatoathaydes <renatoathaydes@hotmail.com>
2019-05-03 21:20:48 +0200

    Re-style examples.

--- example/flattery_example.dart
+++ example/flattery_example.dart
@@ -12,17 +12,19 @@ class CounterView extends Counter with Widget, ShadowWidget {
   final text = Text('')..style.textAlign = 'left';
 
   // use a 2x2 grid to place the Widgets
-  Element build() => Grid(columnGap: '10px', columnWidths: [
-        '10em', '10em' //
-      ], children: [
+  Element build() => Grid(columnGap: '10px', children: [
         [text, text], // row 1 (repeat the element so it takes up both columns)
         [
           Button(text: 'Increment', onClick: (e) => value++),
           Button(text: 'Decrement', onClick: (e) => value--),
         ] // row 2
-      ]).root;
+      ]).root
+        ..classes.add('main-grid');
 
   CounterView() {
+    stylesheet = '* { font-family: sans-serif; }'
+        'button { height: 4em; }'
+        '.main-grid { width: 25em; }';
     _update();
   }
 
--- example/gallery.dart
+++ example/gallery.dart
@@ -5,79 +5,72 @@ import 'package:flattery/flattery_widgets.dart';
 class Gallery with Widget, ShadowWidget {
   Gallery() {
     stylesheet = '''
-    .highlight-flattery-widget { border: solid 1px lightgray; }
+    .grid-header { border-bottom: solid 1px black; 
+                   background: linear-gradient(cadetblue, lightblue); }
+    .grid-cell:not(.grid-header) { margin: 20px; }
     ''';
   }
 
-  void highlightWidgets(bool enable) {
-    querySelectorAllWithShadow('.flattery-widget').forEach((element) async {
-      if (enable)
-        element.classes.add('highlight-flattery-widget');
-      else
-        element.classes.remove('highlight-flattery-widget');
-    });
-  }
-
   @override
-  Element build() => Container(
-        children: [
-          Checkbox(
-            id: 'highlight-flattery-checkbox',
-            label: 'Highlight Flattery Widgets',
-            onChange: highlightWidgets,
-          ),
-          Grid(
-              //
-              columnWidths: [
-                '10em', 'auto' //
+  Element build() {
+    final yellowRectangle =
+        Rectangle(width: '2em', height: '5em', fill: 'yellow');
+
+    return Container(
+      children: [
+        Grid(
+            //
+            columnWidths: [
+              '10em', 'auto' //
+            ],
+            rowGap: '10px',
+            children: [
+              [
+                header('Widget Type'),
+                header('Example')..style.borderLeft = 'solid 1px black'
               ],
-              rowGap: '10px',
-              children: [
-                [
-                  header('Widget Type'),
-                  header('Example')..style.borderLeft = 'solid 1px black'
-                ],
-                [Text('Text'), Text('I am a Text Widget')],
-                [
-                  Text('Checkbox'),
-                  Checkbox(id: 'checkbox-example', label: 'Click to toggle')
-                ],
-                [Text('Button'), Button(text: 'Click me!')],
-                [
-                  Text('Row'),
-                  Row(justify: JustifyContent.spaceEvenly, children: [
-                    Text('This is'),
-                    Text('a Row'),
-                    Text('with 4 Text items'),
-                    Text('spaced evenly'),
-                  ])
-                ],
-                [Text('Rectangle'), Rectangle(fill: 'red', left: '2em')],
-                [
-                  Text('Grid'),
-                  Grid(columnWidths: [
-                    '3em', '3em', '5em' //
-                  ], rowHeights: [
-                    '3em', '3em' //
-                  ], children: [
-                    [
-                      square('blue'),
-                      square('green'),
-                      Rectangle(width: '2em', height: '5em', fill: 'yellow')
-                    ],
-                    [square('red'), square('purple')]
-                  ])
-                ],
-              ])
-            ..style.border = 'solid black 1px'
-            ..style.marginTop = '1em'
-        ],
-      ).root;
+              [Text('Text'), Text('I am a Text Widget')],
+              [
+                Text('Checkbox'),
+                Checkbox(id: 'checkbox-example', label: 'Click to toggle')
+              ],
+              [Text('Button'), Button(text: 'Click me!')],
+              [
+                Text('Row'),
+                Row(justify: JustifyContent.spaceEvenly, children: [
+                  Text('This is'),
+                  Text('a Row'),
+                  Text('with 4 Text items'),
+                  Text('spaced evenly'),
+                ])
+              ],
+              [Text('Rectangle'), Rectangle(fill: 'red', left: '2em')],
+              [
+                Text('Grid'),
+                Grid(columnWidths: [
+                  '3em', '3em', '5em' //
+                ], rowHeights: [
+                  '3em', '3em' //
+                ], children: [
+                  [
+                    // first row
+                    square('blue'), square('green'), yellowRectangle, //
+                  ],
+                  [
+                    // second row
+                    square('red'), square('purple'), yellowRectangle, //
+                  ]
+                ])
+              ],
+            ])
+          ..style.border = 'solid black 1px'
+          ..style.marginTop = '1em'
+      ],
+    ).root;
+  }
 }
 
-Widget header(String text) => Text(text)
-  ..style.backgroundColor = 'lightgray'
-  ..style.borderBottom = 'black solid 1px';
+Widget header(String text) => Text(text)..root.classes.add('grid-header');
 
 Widget square(String color) => Rectangle.square(size: '2em', fill: color);
 
