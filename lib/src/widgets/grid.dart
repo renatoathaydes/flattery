8fc4620627bc02e441dc6ee687baf82e11f0fc86
renatoathaydes <renatoathaydes@hotmail.com>
2019-05-03 21:19:27 +0200

    Proper Grid implementation of elements spanning multiple tracks/rows.

--- lib/src/widgets/grid.dart
+++ lib/src/widgets/grid.dart
@@ -53,15 +53,26 @@ class Grid with Widget {
       root.style.setProperty('grid-column-gap', columnGap);
     }
 
-    for (int row = 0; row < children.length; row++) {
-      final column = children[row];
-      for (int col = 0; col < column.length; col++) {
-        final child = column[col];
+    _findPlacements(children).forEach((child, place) {
+      child.root.classes.add('grid-cell');
+      child.style.gridArea =
+          '${place[0].x} / ${place[0].y} / ${place[1].x} / ${place[1].y}';
+      root.append(child.root);
+    });
+  }
+
+  Map<Widget, List<Point<int>>> _findPlacements(List<List<Widget>> children) {
+    final placements = Map<Widget, List<Point<int>>>();
+    for (int row = 1; row <= children.length; row++) {
+      final column = children[row - 1];
+      for (int col = 1; col <= column.length; col++) {
+        final child = column[col - 1];
         if (child == null) continue;
-        // TODO support specifying items area in the grid
-        child.style.gridArea = '${row + 1} / auto / ${row + 2} / auto';
-        root.append(child.root);
+        final place = placements[child] ?? [Point(row, col), Point(0, 0)];
+        place[1] = Point(row + 1, col + 1);
+        placements[child] = place;
       }
     }
+    return placements;
   }
 }
